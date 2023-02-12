32ecfc41f72ad1203141b460eb70b7445e028fea
Lera <108754223+Linkshegelianer@users.noreply.github.com>
2023-02-12 12:29:24 +0300

    add scrabble()

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -5,5 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
+public class App {
+    public static boolean scrabble(String characters, String word) {
+        ArrayList<Character> charList = new ArrayList<>();
+        for (int i = 0; i < characters.length(); i++) {
+            charList.add(Character.toLowerCase(characters.charAt(i)));
+        }
 
+        for (int i = 0; i < word.length(); i++) {
+            char c = Character.toLowerCase(word.charAt(i));
+            if (!charList.remove((Character) c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 //END
