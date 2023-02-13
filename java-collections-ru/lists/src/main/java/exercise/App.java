e7aedc4d00f8d6eb0ad4d1677285120679907db5
@hexlet/cli <support@hexlet.io>
2023-02-13 13:03:24 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 // BEGIN
 public class App {
-    public static boolean scrabble(String characters, String word) {
-        ArrayList<Character> charList = new ArrayList<>();
-        for (int i = 0; i < characters.length(); i++) {
-            charList.add(Character.toLowerCase(characters.charAt(i)));
-        }
+	public static boolean scrabble(String characters, String word) {
+		ArrayList<Character> charList = new ArrayList<>();
+		for (int i = 0; i < characters.length(); i++) {
+			charList.add(Character.toLowerCase(characters.charAt(i)));
+		}
 
-        for (int i = 0; i < word.length(); i++) {
-            char c = Character.toLowerCase(word.charAt(i));
-            if (!charList.remove((Character) c)) {
-                return false;
-            }
-        }
-        return true;
-    }
+		for (int i = 0; i < word.length(); i++) {
+			char c = Character.toLowerCase(word.charAt(i));
+			if (!charList.remove((Character) c)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 //END
