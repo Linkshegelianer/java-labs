69284051ac6f1e7ed55a684f870852785fc1a621
Lera <108754223+Linkshegelianer@users.noreply.github.com>
2023-03-04 16:20:11 +0300

    remove unnecessary comments

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -10,28 +10,30 @@ class AppTest {
 
     @Test
     void testTake() {
-        // BEGIN
-        List<Integer> test1 = Arrays.asList(1, 2, 3, 4, 5); // создаём массив
-        int n = 3;
-        List<Integer> expectedList1 = Arrays.asList(1, 2, 3); // сами пишем ожидаемый результат
-        List<Integer> resultList1 = Implementations.right(test1, n); // создаём новый массив, который результат метода right
-        assertThat(resultList1).isEqualTo(expectedList1); // сравниваем их
+        // test right()
+        List<Integer> test1 = Arrays.asList(1, 2, 3, 4, 5); // create an array for test
+        int n = 3; // set count value for tests
+        List<Integer> expectedList1 = Arrays.asList(1, 2, 3); // expected result
+        List<Integer> resultList1 = Implementations.right(test1, n); // create a new array containing right() result
+        assertThat(resultList1).isEqualTo(expectedList1); // compare them
 
-        List<Integer> test2 = new ArrayList<Integer>(); // создаём массив
-        List<Integer> expectedList2 = Arrays.asList(0); // сами пишем ожидаемый результат
-        List<Integer> resultList2 = Implementations.wrong1(test2, n); // создаём новый массив, который результат метода wrong1
-        assertThat(resultList2).isEqualTo(expectedList2); // сравниваем их
+        // test wrong1()
+        List<Integer> test2 = new ArrayList<Integer>(); 
+        List<Integer> expectedList2 = Arrays.asList(0); 
+        List<Integer> resultList2 = Implementations.wrong1(test2, n); 
+        assertThat(resultList2).isEqualTo(expectedList2); 
 
-        List<Integer> test3 = new ArrayList<Integer>(Arrays.asList(1, 2));// создаём массив
-        List<Integer> expectedList3 = Arrays.asList(1, 2, 100); // сами пишем ожидаемый результат
-        List<Integer> resultList3 = Implementations.wrong2(test3, n); // создаём новый массив, который результат метода wrong2
-        assertThat(resultList3).isEqualTo(expectedList3); // сравниваем их
+        // test wrong2()
+        List<Integer> test3 = new ArrayList<Integer>(Arrays.asList(1, 2));
+        List<Integer> expectedList3 = Arrays.asList(1, 2, 100); 
+        List<Integer> resultList3 = Implementations.wrong2(test3, n); 
+        assertThat(resultList3).isEqualTo(expectedList3); 
 
-        List<Integer> test4 = new ArrayList<Integer>(Arrays.asList(1, 2, 3)); // создаём массив
-        List<Integer> expectedList4 = Arrays.asList(1, 2, 3, 10); // сами пишем ожидаемый результат
-        List<Integer> resultList4 = Implementations.wrong3(test4, n); // создаём новый массив, который результат метода wrong3
-        assertThat(resultList4).isEqualTo(expectedList4); // сравниваем их
+        // test wrong3()
+        List<Integer> test4 = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+        List<Integer> expectedList4 = Arrays.asList(1, 2, 3, 10);
+        List<Integer> resultList4 = Implementations.wrong3(test4, n); 
+        assertThat(resultList4).isEqualTo(expectedList4); 
 
-        // END
     }
 }
