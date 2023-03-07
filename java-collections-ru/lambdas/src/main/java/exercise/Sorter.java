8b9eb8e65601f03e655d2a5f70eaeff85f5b1bf9
Lera <108754223+Linkshegelianer@users.noreply.github.com>
2023-03-07 18:12:09 +0300

    add comments

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -10,10 +10,10 @@ import java.util.stream.Collectors;
 public class Sorter {
     public static List<String> takeOldestMans(List<Map<String, String>> users) {
         return users.stream()
-                .filter(user -> user.get("gender").equals("male"))
-                .sorted(Comparator.comparing(user -> user.get("birthday")))
-                .map(user -> user.get("name"))
-                .collect(Collectors.toList());
+                .filter(user -> user.get("gender").equals("male")) // find only men
+                .sorted(Comparator.comparing(user -> user.get("birthday"))) // find the biggest birthday which are written like "1980-11-23"
+                .map(user -> user.get("name")) // map each user in the stream to their name
+                .collect(Collectors.toList()); // collect data to a List
     }
 }
 // END
