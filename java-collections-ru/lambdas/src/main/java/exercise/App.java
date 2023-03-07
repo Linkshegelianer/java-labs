0cf3df6c517df63a0d5fa2a4d8948bb28ac6e535
@hexlet/cli <support@hexlet.io>
2023-03-07 18:06:34 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -0,0 +1,11 @@
+package exercise;
+
+import java.util.Arrays;
+
+// BEGIN
+public class App {
+    public static void enlargeArrayImage() {
+
+    }
+}
+// END
---
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -0,0 +1,8 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+// BEGIN
+
+// END
