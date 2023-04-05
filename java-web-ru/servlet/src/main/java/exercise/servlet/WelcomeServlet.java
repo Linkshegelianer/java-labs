ee79e6f4c14e61395b75138254a2705f0dd61bc5
@hexlet/cli <support@hexlet.io>
2023-04-05 16:26:43 +0300

    submit java-web-ru/servlet

---
+++ java-web-ru/servlet/src/main/java/exercise/servlet/WelcomeServlet.java
@@ -0,0 +1,23 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet; // added import for @WebServlet("/")
+
+// BEGIN
+@WebServlet("/")
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.write("Hello, Hexlet!");
+    }
+}
+// END
